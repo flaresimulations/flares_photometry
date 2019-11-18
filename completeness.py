b624a97edd960c31ef4d15398e3615c3ec1ec99e
Aswin Payyoor Vijayan <aswinpvijayan@gmail.com>
2019-11-18 16:42:32 +0000

    read from created luminosity hdf5

--- completeness.py
+++ completeness.py
@@ -1,11 +1,57 @@
 import numpy as np
 import pandas as pd
+import h5py
 import matplotlib.pyplot as plt
+from functools import partial
+import schwimmbad
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-from modules import get_lum_all
 import FLARE.filters
+
+
+def get_data(ii, tag, inp='GEAGLE', filter = 'FUV', Luminosity = 'Dustcorr'):
+
+    if inp == 'GEAGLE':
+
+        num = str(ii)
+
+        if len(num) == 1:
+            num =  '0'+num
+
+        photfile = '../photometry/out/GEAGLE_{}_sp_info.hdf5'.format(num)
+        origfile = '../los/data1/GEAGLE_{}_sp_info.hdf5'.format(num)
+
+    with h5py.File(origfile,'r') as hf:
+        glen = np.array(hf[F"{tag}/Subhalo/G_Length"])
+        slen = np.array(hf[F"{tag}/Subhalo/S_Length"])
+
+    with h5py.File(photfile,'r') as hf:
+        if np.isscalar(filter):
+            lum = np.array(hf[F"{tag}/Subhalo/BPASS/SalpeterIMF/ModelI/Luminosity/{Luminosity}/{filter[8:]}"])
+        else:
+            lum = np.zeros((len(glen), len(filter)))
+            for ii, jj in enumerate(filter):
+                lum[:,ii] = np.array(hf[F"{tag}/Subhalo/BPASS/SalpeterIMF/ModelI/Luminosity/{Luminosity}/{jj[8:]}"])
+
+
+    return lum, glen+slen
+
+
+def get_all(tag, inp = 'GEAGLE', filter = 'FUV', Luminosity = 'Dustcorr'):
+
+    if inp == 'GEAGLE':
+
+        sims = np.arange(0,38)
+
+        calc = partial(get_data, tag = tag, inp = inp, filter = filter, Luminosity = Luminosity)
+
+        pool = schwimmbad.MultiPool(processes=12)
+        dat = np.array(list(pool.map(calc, sims)))
+        pool.close()
+
+        return dat
+
 
 ###Defining input values
 fac = 0.0315
@@ -13,8 +59,7 @@ h = 0.6777
 z = [5, 6, 7, 8, 9, 10]
 tags = ['010_z005p000', '009_z006p000', '008_z007p000', '007_z008p000', '006_z009p000', '005_z010p000']
 tags_ref = ['008_z005p037', '006_z005p971', '005_z007p050', '004_z008p075', '003_z008p988', '002_z009p993']
-filters = FLARE.filters.TH
-
+filters = FLARE.filters.TH[:-1]
 
 df_filter_lims = pd.DataFrame({'z': z})
 percentile = np.empty((len(z), len(filters)))
@@ -22,7 +67,7 @@ percentile = np.empty((len(z), len(filters)))
 
 for ii, jj in enumerate(z):
 
-    data = get_lum_all(fac, tags[ii], LF = False, filters = filters)
+    data = get_all(tags[ii], Luminosity = 'Dustcorr', filter = filters)
 
     for kk in range(38):
 
@@ -39,7 +84,7 @@ for ii, jj in enumerate(z):
 
 for ii, jj in enumerate(filters):
 
-    df_filter_lims[jj] = percentile[:, ii]
+    df_filter_lims[jj[8:]] = percentile[:, ii]
 
 print (df_filter_lims)
 
