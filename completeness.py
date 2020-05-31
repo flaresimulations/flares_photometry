8aa79d39b02ab9d0af972265aa56c17f293cbf30
Aswin Payyoor Vijayan <aswinpvijayan@gmail.com>
2020-05-31 18:28:02 +0100

    completeness limits

--- completeness.py
+++ completeness.py
@@ -4,45 +4,44 @@ import h5py
 import matplotlib.pyplot as plt
 from functools import partial
 import schwimmbad
+from FLARE.photom import lum_to_M
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 import FLARE.filters
 
 
-def get_data(ii, tag, inp='GEAGLE', filter = 'FUV', Luminosity = 'Dustcorr'):
+def get_data(ii, tag, inp='FLARES', filter = 'FUV', Luminosity = 'DustModelI'):
 
-    if inp == 'GEAGLE':
+    if inp == 'FLARES':
 
         num = str(ii)
 
         if len(num) == 1:
             num =  '0'+num
 
-        photfile = '../photometry/out/GEAGLE_{}_sp_info.hdf5'.format(num)
-        origfile = '../los/data1/GEAGLE_{}_sp_info.hdf5'.format(num)
+        filename = './data/flares.hdf5'
 
-    with h5py.File(origfile,'r') as hf:
-        glen = np.array(hf[F"{tag}/Subhalo/G_Length"])
-        slen = np.array(hf[F"{tag}/Subhalo/S_Length"])
+    with h5py.File(filename,'r') as hf:
+        glen = np.array(hf[F"{num}/{tag}/Galaxy/G_Length"])
+        slen = np.array(hf[F"{num}/{tag}/Galaxy/S_Length"])
 
-    with h5py.File(photfile,'r') as hf:
         if np.isscalar(filter):
-            lum = np.array(hf[F"{tag}/Subhalo/BPASS/SalpeterIMF/ModelI/Luminosity/{Luminosity}/{filter[8:]}"])
+            lum = np.array(hf[F"{num}/{tag}/Galaxy/BPASS_2.2.1/Chabrier300/Luminosity/{Luminosity}/{filter[8:]}"])
         else:
             lum = np.zeros((len(glen), len(filter)))
             for ii, jj in enumerate(filter):
-                lum[:,ii] = np.array(hf[F"{tag}/Subhalo/BPASS/SalpeterIMF/ModelI/Luminosity/{Luminosity}/{jj[8:]}"])
+                lum[:,ii] = np.array(hf[F"{num}/{tag}/Galaxy/BPASS_2.2.1/Chabrier300/Luminosity/{Luminosity}/{jj[8:]}"])
 
 
     return lum, glen+slen
 
 
-def get_all(tag, inp = 'GEAGLE', filter = 'FUV', Luminosity = 'Dustcorr'):
+def get_all(tag, inp = 'FLARES', filter = 'FUV', Luminosity = 'DustModelI'):
 
-    if inp == 'GEAGLE':
+    if inp == 'FLARES':
 
-        sims = np.arange(0,38)
+        sims = np.arange(0,40)
 
         calc = partial(get_data, tag = tag, inp = inp, filter = filter, Luminosity = Luminosity)
 
@@ -54,7 +53,6 @@ def get_all(tag, inp = 'GEAGLE', filter = 'FUV', Luminosity = 'Dustcorr'):
 
 
 ###Defining input values
-fac = 0.0315
 h = 0.6777
 z = [5, 6, 7, 8, 9, 10]
 tags = ['010_z005p000', '009_z006p000', '008_z007p000', '007_z008p000', '006_z009p000', '005_z010p000']
@@ -63,13 +61,14 @@ filters = FLARE.filters.TH[:-1]
 
 df_filter_lims = pd.DataFrame({'z': z})
 percentile = np.empty((len(z), len(filters)))
-
+df = pd.read_csv('weight_files/weights_grid.txt')
+weights = np.array(df['weights'])
 
 for ii, jj in enumerate(z):
 
-    data = get_all(tags[ii], Luminosity = 'Dustcorr', filter = filters)
+    data = get_all(tags[ii], Luminosity = 'DustModelI', filter = filters)
 
-    for kk in range(38):
+    for kk in range(len(weights)):
 
         if kk == 0:
             Mlum = data[kk][0]
@@ -80,11 +79,11 @@ for ii, jj in enumerate(z):
 
     ok = np.where(part == 100)
 
-    percentile[ii] = np.percentile(Mlum[ok], 95, axis = 0)
+    percentile[ii] = np.percentile(Mlum[ok], 97, axis = 0)
 
 for ii, jj in enumerate(filters):
 
-    df_filter_lims[jj[8:]] = percentile[:, ii]
+    df_filter_lims[jj[8:]] = lum_to_M(percentile[:, ii])
 
 print (df_filter_lims)
 
